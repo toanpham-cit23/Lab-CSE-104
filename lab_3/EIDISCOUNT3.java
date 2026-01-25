9403e3d58a5977ba2a5599a4cb53e4e19c4aaf50
Pham Song Toan <toan.pham.cit23@eiu.edu.vn>
2026-01-25 15:22:52 +0700

    fixed class name

--- lab_3/EIDISCOUNT3.java
+++ lab_3/EIDISCOUNT3.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EIDISCOUNT3_ {
+public class EIDISCOUNT3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         long money = sc.nextLong();
