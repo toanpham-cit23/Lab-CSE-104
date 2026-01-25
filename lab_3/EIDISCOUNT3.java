6f98ed4418542de4cf34c072cee31b9e6f88619e
Pham Song Toan <toan.pham.cit23@eiu.edu.vn>
2026-01-25 15:06:40 +0700

    fixed to 100 points

--- lab_3/EIDISCOUNT3.java
+++ lab_3/EIDISCOUNT3.java
@@ -3,30 +3,30 @@ import java.util.Scanner;
 public class EIDISCOUNT3_ {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long n = sc.nextLong();
+        long money = sc.nextLong();
         int online = sc.nextInt();
         int vip = sc.nextInt();
         int supersaiyancard = sc.nextInt();
+        long origin = money;
+        long discount = 0;
         long[] thresholds = {900_000_000L, 600_000_000L, 300_000_000L, 100_000_000L, 20_000_000L, 5_000_000L};
-        int[] discountPercents = {15, 12, 10, 7, 5, 3};
-        double totalDiscount = 0;
-        long remaining = n;
+        int[] rates = {15, 12, 10, 7, 5, 3};
 
         for (int i = 0; i < thresholds.length; i++) {
-            if (remaining > thresholds[i]) {
-                long amountInThisLevel = remaining - thresholds[i];
-                totalDiscount += (double) amountInThisLevel * discountPercents[i] / 100.0;
-                remaining = thresholds[i];
+            if (origin > thresholds[i]) {
+                discount += (origin - thresholds[i]) * rates[i] / 100;
+                origin = thresholds[i];
             }
         }
-        double currentBill = (double) n - totalDiscount;
-        if (online == 1)
-            currentBill *= 0.98;
-        if (vip == 1)
-            currentBill *= 0.98;
-        if (supersaiyancard == 1)
-            currentBill *= 0.98;
 
-        System.out.println(Math.round(currentBill));
+        long current = money - discount;
+        if (online == 1)
+            current = current * 98 / 100;
+        if (vip == 1)
+            current = current * 98 / 100;
+        if (supersaiyancard == 1)
+            current = current * 98 / 100;
+
+        System.out.println(current);
     }
 }
