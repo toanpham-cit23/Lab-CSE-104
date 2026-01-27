8090e6ff9db56d7d306dfc660c9adbac225f8210
Pham Song Toan <toan.pham.cit23@eiu.edu.vn>
2026-01-27 13:33:37 +0700

    not complete

--- lab_4/EIDINRATE.java
+++ lab_4/EIDINRATE.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class EIDINRATE  {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        // Sample description
     }
 }
